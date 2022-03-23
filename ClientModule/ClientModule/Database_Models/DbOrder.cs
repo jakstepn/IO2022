fedad745f2b2e2069d1e99793710440b3d660811
Patryk Nikonowicz <01151599@pw.edu.pl>
2022-03-23 07:31:17 +0100

    Added database classes

---
+++ ClientModule/ClientModule/Database_Models/DbOrder.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientModule.Database_Models
+{
+    [DisplayColumn("Order")]
+    [Index(nameof(DbOrder.Date))]
+    [Index(nameof(DbOrder.Status))]
+    public class DbOrder
+    {
+        [Key]
+        public int Id { get; set; } 
+        public decimal Price { get; set; }
+        public virtual DbPayment Payment { get; set; }
+        public double Discount { get; set; }
+        public DbAddress DeliveryAddress { get; set; }
+        public DateTime Date { get; set; }
+        
+        public DbOrderStatus Status { get; set; }
+
+        public bool AddProduct(DbProduct product)
+        {
+            throw new NotImplementedException();
+        }
+        public bool AddFromCart(DbShoppingCart shoppingCart)
+        {
+            throw new NotImplementedException();
+        }
+        public bool ApplyCoupon()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Courier FindCourier()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime EstimateDeliveryTime()
+        {
+            throw new NotImplementedException();
+        }
+        public void NotifyClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        //I do not understand the point of this
+        public DbAddress TrackOrder(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ClientModule/ClientModule/Database_Models/DbShoppingCartEntry.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientModule.Database_Models
+{
+    [DisplayColumn("ShoppingCartEntry")]
+    [Index(nameof(DbShoppingCartEntry.ShoppingCartId))]
+    [Index(nameof(DbShoppingCartEntry.DbProductId))]
+    public class DbShoppingCartEntry
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int ShoppingCartId { get; set; }
+        [Required]
+        public int DbProductId { get; set; }
+
+        public virtual DbShoppingCart ShoppingCart { get; set; }
+        public virtual DbProduct Product { get; set; }
+    }
+}
