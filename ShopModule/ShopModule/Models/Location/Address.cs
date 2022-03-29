aaaf1d2c621e8be8f2956706dcbea43606cc797e
Jakub Stępniewski <01151570@pw.edu.pl>
2022-03-29 17:19:31 +0100

    Implemented basic constructors to the Address struct.

--- ShopModule/ShopModule/Models/Location/Address.cs
+++ ShopModule/ShopModule/Models/Location/Address.cs
@@ -4,10 +4,32 @@ namespace ShopModule.Location
 {
 	public struct Address
 	{
+		public string Country { get; set; }
 		public string City { get; set; }
 		public string Region { get; set; }
 		public string Street { get; set; }
-		public string StreetNumber { get; set; }
-		
+		public int StreetNumber { get; set; }
+		public string ZipCode { get; set; }
+
+		public Address()
+        {
+			City = "";
+			Region = "";
+			Street = "";
+			StreetNumber = -1;
+			ZipCode = "";
+			Country	= "";
+        }
+
+		public Address(string country, string region, string city,
+			string street, int street_number, string zip_code)
+        {
+			City = city;
+			Region = region;
+			Street = street;
+			StreetNumber = street_number;
+			ZipCode = zip_code;
+			Country	= country;
+        }
 	}
 }
