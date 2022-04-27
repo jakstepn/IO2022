5b8c9fabbf1740bdd157f99e17dacd884d103766
Patryk Nikonowicz <pk.nikonowicz@gmail.com>
2022-04-27 15:09:28 +0200

    Added IdentityServer and docker-compose

---
+++ IdentityServer/IdentityServer/Models/Address.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityServer.Models
+{
+    //It was my personal freedom to make it like this.
+    [DisplayColumn("Address")]
+    public class Address
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string PostalCode { get; set; }
+        [Required]
+        //There are quite a lot of addresses with complex street numbers ( 5A, 6/8 etc)
+        public string StreetNumber { get; set; }
+        
+        public int ApartmentNumber { get; set; }
+    }
+}
